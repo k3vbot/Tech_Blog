58c71e461ace20fd648479cedc3f7609577713bd
Kevin <muehl004@gmail.com>
2022-08-15 17:25:37 -0700

    config, schema.sql, and models

---
+++ models/index.js
@@ -0,0 +1,15 @@
+const User = require('./User');
+const Blog = require('./Blog');
+
+Blog.belongsTo(User, {
+    foreignKey: 'userId',
+});
+
+User.hasMany(Blog, {
+    foreignKey: 'userId'
+});
+
+module.exports = {
+    Blog,
+    User,
+};
