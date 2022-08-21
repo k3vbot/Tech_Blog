b8d7a5da8288baab1c651510bbe6ca1711ad4e97
Kevin <muehl004@gmail.com>
2022-08-21 16:06:24 -0700

    started signin.js

---
+++ public/assets/js/signin.js
@@ -0,0 +1,25 @@
+async function signinFormHandler(event) {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-signin').value.trim();
+    const password = document.querySelector('#password-signin').value.trim();
+
+    if (usernmae && password) {
+        const response = await fetch('/api/users/signin', {
+            method: 'post',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if (response.ok) {
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText)
+        }
+    }
+}
+
+document.querySelector('.signin-form').addEventListener('submit', signinFormHandler);
